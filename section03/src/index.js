9fed2b0c9d43449a6fafc67baff74765b4753e40
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-06-15 23:06:56 +0900

    ES Module

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,10 +1,22 @@
+// CommonJS
 // 방법 1
-// const moduleData = require("./math"); 
+// const moduleData = require("./math");
 
-// console.log(moduleData.add(1,2));
-// console.log(moduleData.sub(1,2));
+// console.log(moduleData.add(1, 2));
+// console.log(moduleData.sub(1, 2));
 
 // 방법 2
-const {add, sub} = require("./math");
-console.log(add(1,2));
-console.log(sub(1,2));
+// const {add, sub} = require("./math");
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+
+// ES Module
+import { add, sub } from "./math.js";
+import mul from "./math.js";
+// default 함수는 이름을 변경하고 중괄호가 없어도 된다.
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(2, 3));
+
+// 동일한 경로로부터 값을 받아오는 여러 개의 import 문은 아래처럼 합칠 수 있다.
+// import mul, { add, sub } from "./math.js";
--- section03/src/math.js
+++ section03/src/math.js
@@ -8,7 +8,16 @@ function sub(a, b) {
   return a - b;
 }
 
-module.exports = {
-  add, 
-  sub,
-};
+// commonJS 방식
+// module.exports = {
+//   add, 
+//   sub,
+// };
+
+// ES 모듈 방식
+export { add, sub };
+// 대신 function 앞에 export 키워드를 붙여도 된다.
+// default: 기본 값으로 내보내기 -> 중괄호 없는 새로운 import문이 필요함
+export default function multiply(a, b) {
+  return a * b;
+}
