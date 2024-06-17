68fdcb6180b6250e68a36085b1502f513833a949
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-06-17 18:30:26 +0900

    props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -3,13 +3,24 @@ import Header from "./components/Header.jsx";
 // vite에서는 .jsx 확장자까지는 없어도 된다.
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Button from "./components/Button";
 
 function App() {
+
+  const buttonProps = {
+    text : "메일",
+    color : "red",
+    a : 1,
+    b : 2,
+    c : 3,
+  };
   return (
     <>
-      <Header />
-      <Main />
-      <Footer/>
+      <Button {...buttonProps} />
+      <Button text={"카페"} />
+      <Button text={"블로그"} >
+        <Header />
+      </Button>
     </>
   );
 }
