3f10ab0d86cb997b81838f0e6c4cb3bd0f3c8a96
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-06-18 12:44:29 +0900

    state

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,28 +1,37 @@
 import "./App.css";
-import Header from "./components/Header.jsx";
-// vite에서는 .jsx 확장자까지는 없어도 된다.
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+import {useState} from "react";
 
 function App() {
+  const [count, setCount]= useState(0);
+  const [light, setLight] = useState("OFF");
+  // let light = "OFF" 
+  // JS에서처럼 let 변수를 선언하지 않고 state를 쓰는 이유가 뭘까?
 
-  const buttonProps = {
-    text : "메일",
-    color : "red",
-    a : 1,
-    b : 2,
-    c : 3,
-  };
   return (
-    <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"} >
-        <Header />
-      </Button>
-    </>
-  );
+  <>
+    <div>
+      <h1>{light}</h1>
+      <button 
+        onClick= { () => {
+          setLight(light === "ON"? "OFF" : "ON");
+          // light = light === "ON" ? "OFF" : "ON"
+          // 리렌더링이 되지 않기 때문에 버튼을 아무리 눌러도 상태가 바뀌지 않는다. 
+        }}
+        >
+          {light === 'ON' ? "끄기" : "켜기"} 
+        </button>
+    </div>
+    <div>
+      <h1>{count}</h1>
+      <button 
+        onClick={ ()=> {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+    </div>
+  </>)
 }
 
 export default App;
