b1911739b385b6e6f3ceec79ef28ee44c3bc1385
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-06-19 14:35:13 +0900

    project

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -3,7 +3,9 @@ import { Routes, Route, Link, useNavigate } from "react-router-dom"
 import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
-import Notfound from './pages/NotFount'
+import Notfound from './pages/Notfound'
+
+import { getEmotionImage } from './util/get-emotion-image'
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -11,13 +13,21 @@ import Notfound from './pages/NotFount'
 
 function App() {
   const nav = useNavigate();
-  
+
   const onClickButton = () => {
     nav("/new");
   };
 
   return (
-    <>  
+    <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>  
+
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
@@ -32,7 +42,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
-        <Route path="/diary" element={<Diary />} />
+        <Route path="/diary/:id" element={<Diary />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
   </>
